081c0d281179935c270a2aea826e64279903f2a6
Mark Chavez <markchav3z@gmail.com>
2018-01-10 14:05:47 +0800

    initial commit with subset sum calculator

---
+++ lib/subset_sum.rb
@@ -0,0 +1,30 @@
+class SubsetSum
+  def self.compute(target, options)
+    new(target, options).compute
+  end
+
+  attr_reader :target, :options
+
+  def initialize(target, options)
+    @target = target
+    @options = options
+  end
+
+  def compute
+    [].tap do |result|
+      combination_sum(options.sort.reverse, 0, options.size - 1, target, [], result)
+    end.first
+  end
+
+  private
+
+  def combination_sum(nums, lbound, ubound, target, tracing, result)
+    sum = tracing.reduce(0, &:+)
+    nums[lbound..ubound].each_with_index do |n, i|
+      break if sum + n > target
+
+      result << (tracing.dup << n) if sum + n == target
+      combination_sum(nums, lbound + i, ubound, target, tracing.dup << n, result)
+    end
+  end
+end
---
+++ spec/lib/subset_sum_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require_relative '../../lib/subset_sum'
+
+RSpec.describe SubsetSum do
+  context 'Given a target of `10`' do
+    before(:all) do
+      @target = 10
+    end
+
+    context 'with [5, 10]' do
+      it 'returns one item' do
+        possible_values = [5, 10]
+
+        count = SubsetSum.compute(@target, possible_values).count
+
+        expect(count).to eq 1
+      end
+    end
+
+    context 'with [3, 7, 5]' do
+      it 'returns two items' do
+        count = SubsetSum.compute(@target, [3, 7, 5]).count
+        expect(count).to eq 2
+      end
+    end
+  end
+end
