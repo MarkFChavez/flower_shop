d8346aa6f17576012cd3e406ce1f39594663670d
Mark Chavez <markchav3z@gmail.com>
2018-01-11 09:05:05 +0800

    change variable name

--- start.rb
+++ start.rb
@@ -2,14 +2,14 @@ require_relative './lib/shop'
 
 shop = Shop.new
 
-def print_result(how_many, code, order)
-  puts "#{how_many} #{code} $#{order.total}"
-  order.result.each do |how_many, items|
+def print_result(number_of_pieces, code, order)
+  puts "#{number_of_pieces} #{code} $#{order.total}"
+  order.result.each do |number_of_pieces, items|
     total = items.inject(0) do |total, hash|
       total + hash[:price]
     end
 
-    puts "   #{items.size} x #{how_many} $#{items.first[:price]}"
+    puts "   #{items.size} x #{number_of_pieces} $#{items.first[:price]}"
   end
 end
 
@@ -21,8 +21,8 @@ puts 'Here are the available bundles:'
 shop.bundles.each_with_index do |bundle, index|
   puts "#{index + 1}) [#{bundle.item_name}] Code: #{bundle.item_code}"
   puts '=============================='
-  bundle.price_details.each do |how_many, price|
-    puts "#{how_many} pcs. for $#{price}"
+  bundle.price_details.each do |number_of_pieces, price|
+    puts "#{number_of_pieces} pcs. for $#{price}"
   end
   puts ''
 end
@@ -34,13 +34,13 @@ while try_again do
 
   begin
 
-    how_many, code = gets.split(' ')
-    order = shop.order!(how_many, code)
+    number_of_pieces, code = gets.split(' ')
+    order = shop.order!(number_of_pieces, code)
 
     puts ''
     puts 'Order Details:'
 
-    print_result(how_many, code, order)
+    print_result(number_of_pieces, code, order)
 
     puts ''
     puts 'Do you want to try again? (Y or N):'
